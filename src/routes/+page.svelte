3500c4c9fa743b38f7522a2cbafa5d038e5689a8
Josiah Morris <josiahbmorris@hotmail.com>
2023-07-21 06:08:36 +0900

    Added landing page, and scrolling animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
-<div class="flex flex-col items-center justify-center w-screen h-screen bg-purple-900">
-  <h1 class="text-3xl text-center text-gray-200">Josiah Morris</h1>
-  <span class="text-gray-200 text-md">Personal Page</span>
+<div class="flex flex-col items-center justify-center w-full bg-raisin-500 h-max text-verdigris-500">
+  <section class="flex flex-col items-center justify-center w-full h-screen bg-gradient-to-b from-raisin-500 to-75% to-plum-500">
+    <div class="flex flex-row w-1/2 overflow-hidden border-x-2">
+      <span class="pr-1.5 text-center text-verdigris-500 basis-auto grow-0 shrink-0 animate-scroll">Full stack web developer - Designer - Self guided learner - Lover of purple - Pokemon master - </span>
+      <span class="pr-1.5 text-center text-verdigris-500 basis-auto grow-0 shrink-0 animate-scroll">Full stack web developer - Designer - Self guided learner - Lover of purple - Pokemon master - </span>
+      <span class="pr-1.5 text-center text-verdigris-500 basis-auto grow-0 shrink-0 animate-scroll">Full stack web developer - Designer - Self guided learner - Lover of purple - Pokemon master - </span>
+    </div>
+    <h1 class="w-2/5 my-8 text-5xl italic font-bold text-center md:text-7xl lg:text-9xl text-verdigris-500">Josiah Morris</h1>
+    <div class="flex flex-row w-1/2 overflow-hidden border-x-2 border-verdigris-500">
+      <span class="pr-1.5 text-center text-verdigris-500 basis-auto grow-0 shrink-0 animate-scrollRev">Full stack web developer - Designer - Self guided learner - Lover of purple - Pokemon master - </span>
+      <span class="pr-1.5 text-center text-verdigris-500 basis-auto grow-0 shrink-0 animate-scrollRev">Full stack web developer - Designer - Self guided learner - Lover of purple - Pokemon master - </span>
+      <span class="pr-1.5 text-center text-verdigris-500 basis-auto grow-0 shrink-0 animate-scrollRev">Full stack web developer - Designer - Self guided learner - Lover of purple - Pokemon master - </span>
+    </div>
+  </section>
 </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,16 @@ export default {
           500: '#873582',
         }
       },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)'},
+        }
+      },
+      animation: {
+        'scroll': 'scroll 15s linear infinite',
+        'scrollRev': 'scroll 15s linear infinite reverse'
+      }
     },
   },
   plugins: [],
