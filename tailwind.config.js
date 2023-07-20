b1982644571d94ab4e42ddfdceb5b40f2bafdca2
Josiah Morris <josiahbmorris@hotmail.com>
2023-07-21 05:54:37 +0900

    Extended tailwind config with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        raisin: {
+          500: '#2A222A',
+        },
+        cream: {
+          500: '#E4E7C1',
+        },
+        verdigris: {
+          500: '#7FB9BD',
+        },
+        plum: {
+          500: '#873582',
+        }
+      },
+    },
   },
   plugins: [],
 }
